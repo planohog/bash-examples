dd14e4e5426b29de12f142934d433babd66b84f1
planohog <planohog@gmail.com>
2024-09-09 16:29:36 -0500

    Update osxdeluser.sh
    reworked script

--- osxdeluser.sh
+++ osxdeluser.sh
@@ -2,7 +2,7 @@
 export PATH=/usr/bin:/bin:/usr/sbin:/sbin
 ##################################################
 MLEN=50
-USRNAME=""
+DUSER=""
 DELFLAG=0
 RITNOW=$(date "+%b-%d-%Y %H:%M")
 LOGDIR="/var/root"
@@ -32,17 +32,17 @@ function txtcenter () {
 }
 ##################################################
 function backupuser () {
-UNAME=$1
- if [[ -d "/Users/${UNAME}" ]]; then
+ DELNAME=$1
+ if [[ -d "/Users/${DELNAME}" ]]; then
     echo "${C50}"
-    sudo rsync -av "/Users/${UNAME}" "/var/root/${UNAME}.DELETED" 
-    if [[ -d "/var/root/${UNAME}.DELETED" ]]; then
-      printf ${PF1} "${UNAME} Backed up. /var/root/ " "[OK]" 
-      printf ${PF1} "${UNAME} Backed up. /var/root/ " "[OK]" >> "${LOGDIR}/provision.log"
+    sudo rsync -av "/Users/${DELNAME}" "/var/root/${DELNAME}.DELETED"
+    if [[ -d "/var/root/${DELNAME}.DELETED" ]]; then
+      printf ${PF1} "${DELNAME} Backed up. /var/root/ " "[OK]"
+      printf ${PF1} "${DElNAME} Backed up. /var/root/ " "[OK]" >> "${LOGDIR}/provision.log"
     fi
  else
-    printf ${PF1} "${UNAME} Backed up. /var/root/ " "[FAIL]"
-    printf ${PF1} "${UNAME} Backed up. /var/root/ " "[FAIL]" >>  "${LOGDIR}/provision.log"
+    printf ${PF1} "${DELNAME} Backed up. /var/root/ " "[FAIL]"
+    printf ${PF1} "${DELNAME} Backed up. /var/root/ " "[FAIL]" >>  "${LOGDIR}/provision.log"
  fi
   echo "${C50}"
 }
@@ -50,42 +50,42 @@ UNAME=$1
 function chkdel () {
   if [[ -z ${1} ]]; then echo "HELP [$0 -u UserName -d delete or [$0] "; exit ; fi
   urm=$( echo $1 |tr "[:upper:]" "[:lower:]"  )
-  case $urm in 
+  case $urm in
 	delete ) DELFLAG=1
         ;;
 	del )    DELFLAG=1
         ;;
-	* ) 
+	* )
               echo "${C50}"
 	      printf ${PF1} "Invalid response should be [delete|del]" "[FAIL]"
-	      echo  "HELP [$0 -u UserName -d delete  or [$0] " 
+	      echo  "HELP [$0 -u UserName -d delete  or [$0] "
               echo "${C50}"
 	      exit 1;
 	;;
   esac
 }
 ##################################################
-function deprovisonuser () 
-{ 
+function deprovisionuser ()
+{
   if [[ ${DELFLAG} -gt 0 ]]; then
-    backupuser ${USRNAME}
+    backupuser ${DUSER}
     echo "${C50}"
-    printf ${PF1} "Running sysadminctl -deleteUser [${USRNAME}]"  "[OK]"  
-    $(sysadminctl -deleteUser "${USRNAME}" )
-    echo "sysadminctl -deleteUser ${USRNAME}  ${RITNOW}" >> "${LOGDIR}/provision.log"
+    printf ${PF1} "Running sysadminctl -deleteUser [${DUSER}]"  "[OK]"
+    $(sysadminctl -deleteUser "${DUSER}" )
+    echo "sysadminctl -deleteUser ${DUSER}  ${RITNOW}" >> "${LOGDIR}/provision.log"
     echo "${C50}"
-    if [[ ! -d /Users/${USRNAME} ]]; then
-      printf ${PF1} "Deleted user and user files for [${USRNAME}]"  "[PASS]"
-      printf ${PF1} "Deleted user and user files for [${USRNAME}]"  "[PASS]" >> "${LOGDIR}/provision.log"
+    if [[ ! -d /Users/${DUSER} && -d /var/root/${DUSER}.DELETED  ]]; then
+      printf ${PF1} "Deleted user and user files for [${DUSER}]"  "[PASS]"
+      printf ${PF1} "Deleted user and user files for [${DUSER}]"  "[PASS]" >> "${LOGDIR}/provision.log"
     else
-      printf ${PF1} "Deleted user and user files for [${USRNAME}]"  "[FAIL]"
-      printf ${PF1} "Deleted user and user files for [${USRNAME}]" "[FAIL]" >> "${LOGDIR}/provision.log"
+      printf ${PF1} "Deleted user and user files for [${DUSER}]"  "[FAIL]"
+      printf ${PF1} "Deleted user and user files for [${DUSER}]" "[FAIL]" >> "${LOGDIR}/provision.log"
       exit 1;
     fi
     echo "${C50}"
   else
-    printf ${PF1} "Running sysadminctl -deleteUser [${USRNAME}]"  "[FAIL]"
-    printf ${PF1} "Deleted user and user files for [${USRNAME}]" "[FAIL]" >> "${LOGDIR}/provision.log"
+    printf ${PF1} "Running sysadminctl -deleteUser [${DUSER}]"  "[FAIL]"
+    printf ${PF1} "Deleted user and user files for [${DUSER}]" "[FAIL]" >> "${LOGDIR}/provision.log"
     exit 1;
   fi
  }
@@ -97,30 +97,30 @@ echo "${C50}"
 txtcenter "#"  "SPoC OSX Delete User Script"
 while getopts :u:d:h: FLAG; do
        case ${FLAG} in
-         u) #  del user-name 
-               DUSER="${OPTARG}"       
+         u) #  del user-name
+               DUSER="${OPTARG}"
          ;;
          d) #  delete
                DCOM="${OPTARG}"
 	       chkdel ${DCOM}
          ;;
-        
+
          h) # somekind of help ;;
                HELPME="${OPTARG}"
                echo "HELP [$0 -u UserName -d delete  or [$0] "
-               exit 1 
+               exit 1
 	 ;;
          \?)
             echo "Invalid option: -$OPTARG" >&2
-            exit 1 
+            exit 1
 	 ;;
          :)
             echo "HELP [$0 -u UserName -d delete   or [$0] "
-            exit 1 
+            exit 1
 	 ;;
        esac
 done
 if [ ${HELPME} ]; then exit; fi
-if [ -z ${NLAST} ]; then echo "HELP [$0 -u UserName -d delete  or [$0] "; exit ; fi
+if [ -z ${DUSER} ]; then echo "HELP [$0 -u UserName -d delete  or [$0] "; exit ; fi
 ##################################################
 deprovisionuser
